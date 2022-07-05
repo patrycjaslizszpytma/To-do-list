75d01cab80458f599227da03a549a2083be3c58f
Patrycja Śliż-Szpytma <slizpatrycja@gmail.com>
2022-07-05 12:45:44 +0200

    coments added

--- js/script.js
+++ js/script.js
@@ -11,26 +11,31 @@
     },
   ];
 
+
+
   const removeTask = (taskIndex) => {
     tasks.splice(taskIndex, 1); 
-        //każdy przycisk usuwania będzie usuwał inne zadanie dzięki index
+        //every delete button will be deleting other task thanks to the index value
         render();
   };
 
-  const addNewTask = (newTaskContent) => {
 
-    tasks.push({ //tworzymy obiekt, który jest nowym dodawanym zadaniem
+
+  const addNewTask = (newTaskContent) => {
+    tasks.push({ //creating and pushing an object which is a new task
       content: newTaskContent,
     });
 
     render();
   };
 
+
+
   const render = () => {
-    let htmlString = ""; // na poczatku to jest pusty łańcuch znaków
+    let htmlString = ""; //at the beginning is a empty string chain
 
     for (const task of tasks) {
-      //iterujemy i dodajemy, for of bo nie interesują nas indexy
+      //using for of loop we do an iteration on every task of tasks and an adding, here the indexes does not matter
       htmlString += `
             
                 <li 
@@ -39,17 +44,18 @@
                     <button class="js-remove"> Delete </button>
                     ${task.content}
                 </li>
-            `;
-    }
+            `; //creating a line through decoration for done tasks and sticking a delete button to each task
+    };
+    document.querySelector(".js-tasks").innerHTML = htmlString; // string is putted do the task list
+    //the js-tasks is replaced by the htmlString values which was iterated
 
-    document.querySelector(".js-tasks").innerHTML = htmlString; // string wpisany do listy
 
-    const removeButtons = document.querySelectorAll(".js-remove"); 
-    //przycisk
-    //łapiemy wszytskie przyciski z klasą js-remove
-    //console.log(removeButtons); //NodeList, cos podobnego do tablicy
+
+    const removeButtons = document.querySelectorAll(".js-remove"); //creating the delete button, catching all button with js-remove class
   
-    removeButtons.forEach((removeButton, index) => { //removeButton to przysisk i index to jego lokalizacja, która jest przekazywana do funkcji remove task
+    //console.log(removeButtons); //NodeList, similar to table, for checking
+  
+    removeButtons.forEach((removeButton, index) => { //removeButton is a button and index is it loc that is  transferred to remove task function
 
       removeButton.addEventListener("click", () => {
 
@@ -63,22 +69,23 @@
   const onFormSubmit = (event) => {
     event.preventDefault();
 
-    const newTaskContent = document.querySelector(".js-newTask").value.trim(); //manupulacja trescia zadania
-    // trimem usuwamy białe znaki po bokach
+    const newTaskContent = document.querySelector(".js-newTask").value.trim(); //manupulation the task content, triming the white symbols outside the task
 
     if (newTaskContent === " ") {
-      return; //wychodzę z funkcji jeśli mamy pusty łańcuch znaków
+      return; //do nothing is the string chain is empty
     }
 
-    addNewTask(newTaskContent); //wywołanie funkcji addNewTask
+    addNewTask(newTaskContent); //function call addNewTask
   }
+
+
 
   const init = () => {
     render();
 
-    const form = document.querySelector(".js-form"); //formularz do dodawania nowych zadań
+    const form = document.querySelector(".js-form"); //form for adding new tasks
 
-    form.addEventListener("submit", onFormSubmit); //wywołanie fukncji onFormSubmit na "submit"  
+    form.addEventListener("submit", onFormSubmit); //function call onFormSubmit on "submit"  
 
   };
 
